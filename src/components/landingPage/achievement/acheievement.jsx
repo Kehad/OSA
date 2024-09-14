c7b732feba19b5ceee843c377dfbd3dea40409e4
Kehad <keahnney01@gmail.com>
2024-09-14 23:27:23 +0100

    osa  commit 5

--- src/components/landingPage/achievement/acheievement.jsx
+++ src/components/landingPage/achievement/acheievement.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import image from '../../../assets/achievement-1.png';
+import acheievement2 from "../../../assets/achievement-2.png";
 import ItemsLayout from "../../generalComponent/ItemsLayout";
 import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
 
 function Acheievement() {
   return (
     <ItemsLayout>
-      <Box>
-
+      <Box position={"relative"}>
         <Image src={image} w="600px" h="100%" alt="Image of 3 young students" />
-        <Image src={image} w="600px" h="100%" alt="Image of 3 young students" />
+        <Image src={acheievement2} w="300px" h="50%" alt="Image of 3 young students" objectFit={"cover"} position={"absolute"} bottom={"-13%"} left={"50%"}/>
       </Box>
       <Box>
         <Heading
