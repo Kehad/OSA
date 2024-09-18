a5a4dd8040a80b476707ffe4a2816e549c44760a
Kehad <keahnney01@gmail.com>
2024-09-18 13:31:02 +0100

    osa  commit 19

--- src/components/aboutUsPage/story/Story.jsx
+++ src/components/aboutUsPage/story/Story.jsx
@@ -11,21 +11,24 @@ function Story() {
       gap={4}
     >
       <Heading>Story</Heading>
-      <Text width="70%">
-        Oluwatosin Success Academy, OSA, is a school that is established as
-        modern age educational centre with {" \n "} international standard to
-        provide excellent education at the elementary level, so as to contribute
-        to the achievement {" \n "} of the development goal in Nigeria.{" \n "}{" "}
-        ​The Primary section of the school started officially on the 15th of
-        September, 2007 while the Secondary school {" \n "} section was birthed
-        and began operation on the 7th of September, 2015. {" \n "} ​The two
-        major motivating factors for establishing the schools are: {" \n "}{" "}
-        1.​The need to contribute to the educational development of Pupils
-        through sound education especially at the elementary level. {" \n "}{" "}
-        2.​The need to provide solid foundation for pupils and students in every
-        aspect of life (Spiritually, Morally {" \n "} and Educationally){" "}
-        {" \n "} We are currently located in two States of the Federal Republic
-        of Nigeria
+      <Text
+        width="70%"
+        wordBreak="break-word"
+        whiteSpace="pre-wrap"
+        textAlign="center"
+      >
+        {`Oluwatosin Success Academy, OSA, is a school that is established as modern age educational centre with  
+        international standard to provide excellent education at the elementary level, so as to contribute to the achievement 
+of the development goal in Nigeria.
+​The Primary section of the school started officially on the 15th of September, 2007 while the Secondary school 
+section was birthed and began operation on the 7th of September, 2015. 
+​The two major motivating factors for establishing the schools are: 
+1.​The need to contribute to the educational development of Pupils through sound education especially 
+at the elementary level. 
+2.​The need to provide solid foundation for pupils and students in every aspect of life (Spiritually, Morally and
+Educationally)
+We are currently located in two States of the Federal Republic of Nigeria`
+              }
       </Text>
     </Flex>
   );
